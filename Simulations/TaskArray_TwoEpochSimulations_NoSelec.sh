195d321cef780d110496147580d11763fda299d7
amzurita <aqua1313@gmail.com>
2024-10-08 16:30:50 -0700

    TwoEpoch Debug

---
+++ Simulations/TaskArray_TwoEpochSimulations_NoSelec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#EDIT THIS LINES:
+#$ -N TwoEpochSimulations_NoSynSel_$JOB_ID.$TASK_ID
+#$ -o /u/scratch/a/amzurita/logs/out_TwoEpochSimulations_NoSynSel_$JOB_ID.$TASK_ID.txt
+
+#$ -j y
+#$ -cwd
+#$ -M amzurita
+#$ -l highp
+#$ -l h_data=16G
+#$ -l time=72:00:00
+#$ -m bea
+
+#EDIT THIS LINE WITH THE NUMBER OF SAMPLES (See the trick at the end of the script to count)
+#$ -t 1-1000
+
+. /u/local/Modules/default/init/modules.sh
+
+/u/project/klohmuel/amzurita/Tools/slim_build/slim -d "run_name_output='twoepochsim_nosel_debug${SGE_TASK_ID}'" TwoEpochNoSynSelection.slim
